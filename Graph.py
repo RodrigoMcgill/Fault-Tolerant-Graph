f963cac387b240a958fdb8ca757da409570d7e17
rodrigo-mendoza <rodrigo.mendozaarbizu@mail.mcgill.ca>
2017-04-09 22:53:49 -0400

    added try except

--- Graph.py
+++ Graph.py
@@ -2,108 +2,132 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import operator
 import os
-
+import pandas as pd
 
 G = nx.Graph()
 
-#Draw a node given the |Graph 'G'|node|position|
-def draw_node(G, node,x,y):
-    G.add_node(node, id=node, position=(x,y))
 
-#Draw a edge given the |Graph 'G'|source node|target node|reliability|
-def draw_edge(G,source,target,r):
+# Draw a node given the |Graph 'G'|node|position|
+def draw_node(G, node, x, y):
+    G.add_node(node, id=node, position=(x, y))
+
+
+# Draw a edge given the |Graph 'G'|source node|target node|reliability|
+def draw_edge(G, source, target, r):
     G.add_edge(source, target, r=r)
 
-#Automatically draws and plots the graph G
+
+# Automatically draws and plots the graph G
 def draw_graph(G):
     pos = nx.get_node_attributes(G, 'position')
     nx.draw(G, pos, node_size=2000, node_color='black')
     node_labels = nx.get_node_attributes(G, 'id')
     nx.draw_networkx_labels(G, pos, labels=node_labels, font_color='w')
-    edge_labels = nx.get_edge_attributes(G, 'id')
+    edge_labels = nx.get_edge_attributes(G,'id')
     nx.draw_networkx_edge_labels(G, pos, labels=edge_labels)
     plt.show()
 
-#Obtain all simple paths between source node and target node
+
+# Obtain all simple paths between source node and target node
 def modify_djikstra(G, source, target):
-      paths =list(nx.all_simple_paths(G, source=source, target=target))
-      reliabilities = []
-      for p in paths:
-          tolRel=1
-          for i in range(len(p)-1):
-              tolRel = tolRel*G[p[i]][p[i+1]]['r']
-          reliabilities.append(tolRel)
-      path_sorted = []
-      reliabilities_sorted= []
-      for i in range(len(reliabilities)):
-            index, value = max(enumerate(reliabilities), key=operator.itemgetter(1))
-            print("index: "+str(index)+" value"+str(value))
-            print(paths[index])
-            path_sorted.append(paths[index])
-            reliabilities_sorted.append(value)
-            del paths[index]
-            del reliabilities[index]
-      return  path_sorted,reliabilities_sorted
+    paths = list(nx.all_simple_paths(G, source=source, target=target))
+    reliabilities = []
+    for p in paths:
+        tolRel = 1
+        for i in range(len(p) - 1):
+            tolRel = tolRel * G[p[i]][p[i + 1]]['r']
+        reliabilities.append(tolRel)
+    path_sorted = []
+    reliabilities_sorted = []
+    for i in range(len(reliabilities)):
+        index, value = max(enumerate(reliabilities), key=operator.itemgetter(1))
+        print("index: " + str(index) + " value" + str(value))
+        print(paths[index])
+        path_sorted.append(paths[index])
+        reliabilities_sorted.append(value)
+        del paths[index]
+        del reliabilities[index]
+    return path_sorted, reliabilities_sorted
+
 
 def clear():
-    cls = '\n'*50
+    cls = '\n' * 5
     print(cls)
 
-#Create nodes and edges
-def arguments():
+
+# Create nodes and edges
+def Djikstra_arguments():
     done = 'yes'
     while done == 'yes':
-        name, x,y = input("Create a node [name of node] [position x] [position y] \n").split(' ')
-        draw_node(G, name,int(x),int(y))
-        done = input("continue?: [yes] [no] \n")
-        if done !='no':
-            if done !='yes':
-                done = 'yes'
-        clear()
+        try:
+            name, x, y = input("Create a node [name of node] [position x] [position y] \n").split(' ')
+            draw_node(G, name, int(x), int(y))
+            done = input("continue?: [yes] [no] \n")
+            if done != 'no':
+                if done != 'yes':
+                    done = 'yes'
+            clear()
+        except:
+            print('Oops, an error occurred, please try again \n')
     done = 'yes'
     while done == 'yes':
-        print("This is a list of all nodes")
-        print(G.nodes(data=False))
-        source, target, r = input("Which nodes you want to connect and what's the reliability [Source] [Target] [Reliability] \n").split(' ')
-        draw_edge(G,source,target,r)
-        done = input("continue?: [yes] [no] \n")
-        clear()
+        try:
+            print("This is a list of all nodes")
+            print(G.nodes(data=False))
+            source, target, r = input("Which nodes you want to connect and what's the reliability [Source] [Target] [Reliability] \n").split(' ')
+            draw_edge(G, source, target, r)
+            done = input("continue?: [yes] [no] \n")
+            clear()
+        except:
+            print('Oops, an error occurred, please try again \n')
+
+
+def prim_optimization(inp, mode):
+    inp = pd.read_excel('Input.xlsx')
+
+    if mode == "Cost":
+        inp.sort_values('Cost', ascending=True, inplace=True)
+        print(inp)
+    else:
+        inp.sort_values('Reliability', ascending=False, inplace=True)
+        print(inp)
+
+    for index, row in inp.iterrows():
+        nameA = row['First Edge']
+        nameB = row['Second Edge']
+
+        G.add_node(nameA, id=nameA)
+        G.add_node(nameB, id=nameB)
+        if not nx.has_path(G, nameA, nameB):
+            G.add_edge(nameA, nameB, r=row['Reliability'])
+
+
+def prim_arguments():
+    inp, mode = input(
+        "Copy the Path of the excel file you would like to use, and the optimization parameter [Path] [Cost or Reliability]\n").split(
+        ' ')
+    prim_optimization(inp, mode)
+
+    nx.draw_networkx(G)
+    plt.show()
 
 
 def main():
-    arguments()
-    draw_graph(G)
+    while True:
+        select = input("Select mode of Operation [Prim or Djikstra] \n")
+        if select == "Prim":
+            prim_arguments()
+        elif select == "Djikstra":
+            Djikstra_arguments()
+            draw_graph(G)
+        else:
+            print('Oops, there was an error in the input, please write correctly the method \n')
 
 
 main()
 
-
 """
-
-
 x,y= input("fff \n").split(' ')
-name= input("enter for x \n")
-G.add_node(name, id=name, position=(int(x),int(y)))
-G.add_node('V', id='V', position= (2,2))
-G.add_node('X', id='X', position=(2, -2))
-G.add_node('W', id='W', position=(4, 2))
-G.add_node('Y', id='Y', position=(4, -2))
-G.add_node('Z', id='Z', position=(6, 0))
-# Defines edges between nodes and their weights
-G.add_edge('U', 'V', r=2)
-G.add_edge('U', 'X', r=1)
-G.add_edge('U', 'W', r=5)
-G.add_edge('V', 'W', r=3)
-G.add_edge('V', 'X', r=2)
-G.add_edge('X', 'W', r=3)
-G.add_edge('X', 'Y', r=1)
-G.add_edge('W', 'Y', r=1)
-G.add_edge('W', 'Z', r=5)
-G.add_edge('Y', 'Z', r=3)
-
-draw_graph(G)
-
-
 path_sorted,reliabilities_sorted = modify_djikstra(G,'U','Z')
 print("path sorted \n:" + str(path_sorted)+ "\n reliabilities sorted" + str(reliabilities_sorted))
 """
